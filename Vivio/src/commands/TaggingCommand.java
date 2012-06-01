9779161ee9846d93f9c1abe66299212f115805db
imnothere199 <imnothere199@2da491a5-97c4-c799-e8a7-435aa7d697a3>
2012-06-01 03:41:00 +0000

    Played with the tag message format.
    git-svn-id: https://vivio.googlecode.com/svn/trunk@33 2da491a5-97c4-c799-e8a7-435aa7d697a3

--- Vivio/src/commands/TaggingCommand.java
+++ Vivio/src/commands/TaggingCommand.java
@@ -1,10 +1,12 @@
 package commands;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 
 import org.pircbotx.Channel;
+import org.pircbotx.Colors;
 import org.pircbotx.User;
 
 import backend.Bot;
@@ -54,43 +56,54 @@ public class TaggingCommand extends Command {
 		if(alias.equals("tag")) {
 			execute(bot, chan, user, message);
 			return;
+		}else if(alias.equals("tag-change")) {
+			tagChange(bot, chan, user, message);
+		}else if(alias.equals("tag-browse")) {
+			tagBrowse(bot, chan, user, message);
 		}
-		if(alias.equals("tag-change")) {
-			if(!Util.hasArgs(message, 3)) {
-				invalidFormat(bot, chan, user);
-				return;
-			}
-			String[] args = Util.getArgs(message, 3);
+	}
+
+	private void tagBrowse(Bot bot, Channel chan, User user, String message) {
+		if(!Util.hasArgs(message, 2)) {
+			invalidFormat(bot, chan, user);
+			return;
+		}
+		String[] args = Util.getArgs(message, 2);
+		List<HashMap<String, Object>> results = null;
+		try {
+			if(args[1].contains("%")) 
+				results = Database.select("select * from "+getFormattedTableName()+" where tag like '"+args[1]+"'");
+			else
+				results = Database.select("select * from "+getFormattedTableName()+" where tag = '"+args[1]+"'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if(results == null) {
+			passMessage(bot, chan, user, "No results for "+args[1]);
+			return;
+		}
+		for(HashMap<String, Object> row : results) {
+			String url = (String) row.get("URL");
 			try {
-				Database.execRaw("update "+getFormattedTableName()+" set tag='"+args[2]+"' where tag='"+args[1]+"'");
-			} catch (SQLException e) {
+				passMessage(bot, chan, user, modules.LinkParsingModule.parseLink(url) + " ("+url+")" + " [" + Colors.BOLD + row.get("TAG").toString().trim() + Colors.NORMAL + "]");
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			passMessage(bot, chan, user, "Changed all "+args[1]+" to "+args[2]);
 		}
-		if(alias.equals("tag-browse")) {
-			if(!Util.hasArgs(message, 2)) {
-				invalidFormat(bot, chan, user);
-				return;
-			}
-			String[] args = Util.getArgs(message, 2);
-			List<HashMap<String, Object>> results = null;
-			try {
-				if(args[1].contains("%")) 
-					results = Database.select("select * from "+getFormattedTableName()+" where tag like '"+args[1]+"'");
-				else
-					results = Database.select("select * from "+getFormattedTableName()+" where tag = '"+args[1]+"'");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			if(results == null) {
-				passMessage(bot, chan, user, "No results for "+args[1]);
-				return;
-			}
-			for(HashMap<String, Object> row : results) {
-				passMessage(bot, chan, user, row.get("URL") + " [" + row.get("TAG").toString().trim() + "]");
-			}
+	}
+
+	private void tagChange(Bot bot, Channel chan, User user, String message) {
+		if(!Util.hasArgs(message, 3)) {
+			invalidFormat(bot, chan, user);
+			return;
 		}
+		String[] args = Util.getArgs(message, 3);
+		try {
+			Database.execRaw("update "+getFormattedTableName()+" set tag='"+args[2]+"' where tag='"+args[1]+"'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		passMessage(bot, chan, user, "Changed all "+args[1]+" to "+args[2]);
 	}
 
 	@Override
