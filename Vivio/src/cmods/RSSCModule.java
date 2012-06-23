92bf7887763620a7c4aa89cc72d1dc5296016b2a
imnothere199@gmail.com <imnothere199@gmail.com@2da491a5-97c4-c799-e8a7-435aa7d697a3>
2012-06-23 16:06:07 +0000

    Another todo for RSSCmodule
    git-svn-id: https://vivio.googlecode.com/svn/trunk@87 2da491a5-97c4-c799-e8a7-435aa7d697a3

--- Vivio/src/cmods/RSSCModule.java
+++ Vivio/src/cmods/RSSCModule.java
@@ -32,6 +32,7 @@ import backend.Util;
 public class RSSCModule extends Command {
 	
 	private final int RSS_CHECK_TIME = 600;
+   //TODO change this back to a limitedqueue of size 100 or something. Maybe 200.
 	private HashMap<String, LinkedList<String>> mostRecent = new HashMap<>();
 	
 	private static SyndFeedInput input = new SyndFeedInput();
