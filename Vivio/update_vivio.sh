52dcbca6b3ef263f6616e755fe71393167492f6f
imnothere199 <imnothere199@2da491a5-97c4-c799-e8a7-435aa7d697a3>
2012-08-10 01:01:24 +0000

    shell script works perfectly boss.
    git-svn-id: https://vivio.googlecode.com/svn/trunk@133 2da491a5-97c4-c799-e8a7-435aa7d697a3

--- Vivio/update_vivio.sh
+++ Vivio/update_vivio.sh
@@ -1,8 +1,20 @@
 #! /bin/sh
 
-#wget
-#ant -buildfile build.xml
-#ant -buildfile jar.xml
+#remove old vivio
+rm -r vivio.googlecode.com/
+
+#wget new version
+wget -r https://vivio.googlecode.com/svn/trunk
+
+#build binary information
+ant -buildfile vivio.googlecode.com/svn/trunk/Vivio/build-bin.xml
+
+#build updated jar
+ant -buildfile vivio.googlecode.com/svn/trunk/Vivio/build-jar.xml
+
 #cp jar to current directory
+cp vivio.googlecode.com/svn/trunk/Vivio/vivio.jar .
+
 #launch jar
+java -jar vivio.jar -o Seiyria -c kellyirc
 
