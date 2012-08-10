5550d364771f099380522f0a2d6269b12436a7d3
imnothere199 <imnothere199@2da491a5-97c4-c799-e8a7-435aa7d697a3>
2012-08-10 02:02:21 +0000

    can't have the bot updating multiple times at once. that'd be catastrophic. STATE MACHINES FOR THE WIN. ..I guess.
    git-svn-id: https://vivio.googlecode.com/svn/trunk@137 2da491a5-97c4-c799-e8a7-435aa7d697a3

--- Vivio/src/commands/UpdateCommand.java
+++ Vivio/src/commands/UpdateCommand.java
@@ -10,10 +10,21 @@ import org.pircbotx.User;
 import backend.Bot;
 
 public class UpdateCommand extends Command {
+	
+	private boolean updating = false;
 
 	@Override
 	public void execute(Bot bot, Channel chan, User user, String message) {
 
+		if(updating) {
+			passMessage(bot, chan, user, "I am already updating. Please be patient.");
+			return;
+		}
+		
+		passMessage(bot, chan, user, "I will begin updating now. Please give me some time.");
+		
+		updating = true;
+		
 		Process p = null;
 		try {
 			p = Runtime.getRuntime().exec("./update_vivio.sh");
